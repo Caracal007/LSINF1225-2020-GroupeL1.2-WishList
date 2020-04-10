ec8be8e4563e781a2c1c5886ac40db2b21c6919a
mmomin <maxime.momin@student.uclouvain.be>
2020-04-11 01:24:35 +0200

    Application + Inscription_page (Updated)

--- app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainActivity.java
@@ -3,6 +3,9 @@ package be.uclouvain.lsinf1225.groupel12.wishlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lol();
+
+    }
+    private void lol(){
+        ((Button) findViewById(R.id.btnInscription)).setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "mdr", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
