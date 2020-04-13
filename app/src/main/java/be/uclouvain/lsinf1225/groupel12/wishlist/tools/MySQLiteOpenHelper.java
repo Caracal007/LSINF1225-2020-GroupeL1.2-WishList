9ffb6a93eab0a01f01b273fe28e88d62014ed95e
mmomin <maxime.momin@student.uclouvain.be>
2020-04-13 16:27:37 +0200

    Test

--- app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/tools/MySQLiteOpenHelper.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/tools/MySQLiteOpenHelper.java
@@ -64,4 +64,5 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
             }
     }
     //Inscription ------------------------------------------------------------------>>>
+    //Maxime
 }
