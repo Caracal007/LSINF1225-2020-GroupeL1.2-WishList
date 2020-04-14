699b76eba4ed9f4dc0670d93de51aa41400b4e46
mmomin <maxime.momin@student.uclouvain.be>
2020-04-14 19:35:32 +0200

    +Page profil.xml

--- app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainPreferences.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainPreferences.java
@@ -2,7 +2,9 @@ package be.uclouvain.lsinf1225.groupel12.wishlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainPreferences extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainPreferences extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_preferences);
+
+        Button inscription = findViewById(R.id.buttonMajPreferences);
+        inscription.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, MainProfil.class);
+
+            startActivity(intent);
+        });
 
     }
     @Override
