6dd2d7edac13bf36e067f612014eb3210b48da6d
mmomin <maxime.momin@student.uclouvain.be>
2020-04-22 13:09:44 +0200

    Update Profil/ModifProfil

--- app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainModifProfil.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainModifProfil.java
@@ -1,9 +1,32 @@
 package be.uclouvain.lsinf1225.groupel12.wishlist;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainModifProfil extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_preferences);
+        logout();
+    }
+    /* Button ---------------------------------------------------------------- */
+    private void logout() {
+        findViewById(R.id.buttonMajPreferences).setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                openActivityLogin();
+            }
+        })
+        ;}
+    public void openActivityLogin(){
+        Intent intent = new Intent(this, MainLogin.class);
+        startActivity(intent);
+    }
+    /* Button ---------------------------------------------------------------- */
 }
 
 
