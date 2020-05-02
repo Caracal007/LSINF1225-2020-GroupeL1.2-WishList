cbc21f1d243c18501b9a84dac502d477c7b3491f
mathiasc <corentin.mathias@student.uclouvain.be>
2020-05-02 10:44:16 +0200

    content wishlist .xml commencé

--- app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainContentWishList.java
+++ app/src/main/java/be/uclouvain/lsinf1225/groupel12/wishlist/MainContentWishList.java
@@ -9,6 +9,6 @@ public class MainContentWishList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_content_wish_list);
+        setContentView(R.layout.activity_main_content_wishlist);
     }
 }
